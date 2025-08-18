006dc06e4a07cd553b8bdf48522f6090e88225bd
unknown <somuddro249@gmail.com>
2025-08-18 15:01:27 +0600

    update

--- app/Service/Excel.php
+++ app/Service/Excel.php
@@ -5,9 +5,10 @@ namespace App\Service;
 class Excel
 {
 
+
     public static function export($output , $filename){
         return response($output)
-            ->header('Content-Type', 'text/csv')
+            ->header('Content-Type', 'application/csv')
             ->header('Content-Disposition', 'attachment; filename="' . $filename . '"')
             ->header('Pragma', 'no-cache')
             ->header('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
